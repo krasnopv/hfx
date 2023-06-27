687f69d86c29427c088d5766566ccb981c911d0b
abriandev <krasnopv@gmail.com>
2023-06-27 23:17:55 +0100

    Header, Hero

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,19 +1,28 @@
 "use strict";
 
 $(document).ready(function () {
-  $('body').autoPadding({
-    source: $('.js-header')
+  $("body").autoPadding({
+    source: $(".js-header")
   });
-  //removeIf(production)
-  console.log("document ready");
-  //endRemoveIf(production)
+  console.log("Ready!");
 });
-
-$(window).on('load', function () {
-  //for use in production please remove this setTimeOut
+$(window).on("load", function () {
+  var navHeight = parseInt($("nav").css("height"));
+  var sectionMarginTop = parseInt($("section").css("margin-top"));
+  var navMarginTop = parseInt($("nav").css("margin-top"));
+  // $('.preloader').addClass('preloader-deactivate');
+  $(".wrapper").removeClass("invisible");
   setTimeout(function () {
-    $('.preloader').remove();
-  }, 3000);
-  //uncomment this line for use this snippet in production
-  //	$('.preloader').addClass('preloader-deactivate');
+    $(".preloader").remove();
+  }, 500);
+  var titleMarginTop = parseInt($("#hero2 h1").css("margin-bottom")) + parseInt($("#hero2 button").css("height"));
+  $("#hero2 h1").css({
+    "margin-top": titleMarginTop
+  });
+  var mainTop = 0 - navHeight - navMarginTop - sectionMarginTop;
+  $("main").css({
+    'top': mainTop,
+    'margin-top': 0 - sectionMarginTop
+  });
+  console.log("Loaded!");
 });
